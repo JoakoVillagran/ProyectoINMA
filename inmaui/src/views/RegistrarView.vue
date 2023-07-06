ca332b00883fd53ff5fcfe6ece721e0727d5daf2
SHUBAAA <m.alvial03@ufromail.cl>
2023-07-05 20:37:57 -0400

    Registra usuario

--- inmaui/src/views/RegistrarView.vue
+++ inmaui/src/views/RegistrarView.vue
@@ -5,38 +5,40 @@
         </div>
     </nav>
 
-    <div class="container justify-content-center ">
+    <div class="container justify-content-center">
         <div class="row justify-content-center">
             <div class="col-md-6 col-lg-5">
                 <div class="login-wrap p-4 p-md-5">
-                    <form>
+                    <form @submit.prevent="registrarUsuario">
                         <input type="hidden" />
                         <div class="form-group">
                             <label class="col-form-label">Email</label>
-                            <input type="email" class="form-control" placeholder="Ingrese su Email" required />
+                            <input type="email" class="form-control" v-model="usuario.email" placeholder="Ingrese su Email"
+                                required />
                         </div>
                         <div>
-                            <label class="col-form-label">Contrase&ntilde;a</label>
-                            <input type="password" class="form-control" required minlength="2" maxlength="10"
-                                placeholder="Ingrese su Contrase&ntilde;a" />
+                            <label class="col-form-label">Contraseña</label>
+                            <input type="password" class="form-control" v-model="usuario.contrasena" required minlength="2"
+                                maxlength="10" placeholder="Ingrese su Contraseña" />
                         </div>
                         <div>
-                            <label class="col-form-label"> Nombre</label>
-                            <input type="text" class="form-control" required minlength="2" maxlength="20"
-                                placeholder="Ingrese su Nombre" />
+                            <label class="col-form-label">Nombre</label>
+                            <input type="text" class="form-control" v-model="usuario.nombre" required minlength="2"
+                                maxlength="20" placeholder="Ingrese su Nombre" />
                         </div>
-
                         <div>
                             <label class="col-form-label">Rut</label>
-                            <input type="text" class="form-control" required minlength="2" maxlength="20"
-                                placeholder="Ingrese su Rut" />
+                            <input type="text" class="form-control" v-model="usuario.rut" required minlength="2"
+                                maxlength="20" placeholder="Ingrese su Rut" />
                         </div>
                         <div>
-                            <label class="col-4 col-form-label">Roles: </label>
+                            <label class="col-4 col-form-label">Roles:</label>
                             <div class="col-8">
-
-                                <input type="checkbox" class="m-2" />
-
+                                <select v-model="usuario.rol" class="form-control">
+                                    <option value="CAJERO">Cajero</option>
+                                    <option value="BODEGUERO">Bodeguero</option>
+                                    <option value="ADMIN">Admin</option>
+                                </select>
                             </div>
                         </div>
 
@@ -52,8 +54,42 @@
         </div>
     </div>
 </template>
-
+  
 <script>
-</script>
+import axios from 'axios';
 
+export default {
+    data() {
+        return {
+            usuario: {
+                email: '',
+                contrasena: '',
+                nombre: '',
+                rut: '',
+                rol: ''
+            },
+
+        };
+    },
+    methods: {
+        registrarUsuario() {
+            axios.post('http://localhost:3000/registrar', this.usuario)
+                .then(response => {
+                    console.log(response)
+                    console.log('Usuario registrado con éxito');
+                    this.$router.push('/admin')
+                    // Lógica adicional después de registrar el usuario
+                })
+                .catch(error => {
+
+                    alert(error.response.data.message)
+                    console.error('Error al registrar el usuario:', error);
+                    // Lógica de manejo de error
+                });
+        }
+    }
+};
+</script>
+  
 <style></style>
+  
